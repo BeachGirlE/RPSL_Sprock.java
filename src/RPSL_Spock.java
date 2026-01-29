98ba57a21ab6e841970b0325b2d4f9281e59682e
eemorris <eemorris@thewildcats.net>
2026-01-29 11:59:13 -0500

    String Generator

--- src/RPSL_Spock.java
+++ src/RPSL_Spock.java
@@ -1,4 +1,14 @@
+import java.util.Random;
+
 public class RPSL_Spock {
     public static void main(String[] args) {
+        // Computer's input code:
+            // Naming the options for String output
+        String[] options = {"rock", "paper", "scissors", "lizard", "SPOCK!"};
+        Random random = new Random();
+
+
+        int select = random.nextInt(options.length);
+        System.out.println("Computer's choice: " + options[select]);
     }
 }
