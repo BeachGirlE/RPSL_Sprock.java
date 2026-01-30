8cc7fd971928a0d1469e3d2bfa5f5f43a2a3d645
eemorris <eemorris@thewildcats.net>
2026-01-29 20:19:26 -0500

    Intro and other edits

--- src/RPSL_Spock.java
+++ src/RPSL_Spock.java
@@ -1,14 +1,34 @@
+// import java.lang.String;
+import java.util.Scanner;
 import java.util.Random;
-
 public class RPSL_Spock {
     public static void main(String[] args) {
+        // Intro to the game:
+        System.out.println("Please enter your name.");
+        Scanner scanner = new Scanner(System.in);
+        String userName = scanner.nextLine();
+        System.out.println("Welcome " + userName + ". You are about to play a game of 'Rock, Paper, Scissors, Lizard, SPOCK!'");
+
+        //Prompt user to enter a choice
+        System.out.println("Please choose one of the following options: Rock, Paper, Scissors, Lizard, or Spock." +
+                " Please keep it in all lowercase!");
+        String userMove = scanner.nextLine().toLowerCase().trim();
+
         // Computer's input code:
-            // Naming the options for String output
-        String[] options = {"rock", "paper", "scissors", "lizard", "SPOCK!"};
+        // Naming the options for String output
+        String[] options = {"rock", "paper", "scissors", "lizard", "spock"};
         Random random = new Random();
 
-
+        // Printing everything out!!
         int select = random.nextInt(options.length);
-        System.out.println("Computer's choice: " + options[select]);
+        System.out.println("Computer's play: " + options[select]);
+
+        // write conditionals for who wins and who gains points!!
+        if (userMove.equals(random)) {
+            System.out.println("It's a tie");
+        } else {
+            System.out.println("Not a tie");
+            //((userMove.equals("scissors") && random.equals("paper")));
+        }
     }
 }
