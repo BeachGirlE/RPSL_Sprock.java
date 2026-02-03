d5adb101b6e3872df6a2d4f2df359306d801a735
tshart <tshart@thewildcats.net>
2026-02-03 11:29:05 -0500

    updated code format

--- src/RPSL_Spock.java
+++ src/RPSL_Spock.java
@@ -1,5 +1,6 @@
 import java.util.Random;
 import java.util.Scanner;
+
 public class RPSL_Spock {
     public static void main(String[] args) {
         // Intro to the game:
@@ -8,7 +9,8 @@ public class RPSL_Spock {
         System.out.println("Please enter your name.");
         Scanner scanner = new Scanner(System.in);
         String userName = scanner.nextLine();
-        String userMove = scanner.nextLine().toLowerCase().trim();;
+        String userMove = scanner.nextLine().toLowerCase().trim();
+        ;
         System.out.println("Welcome " + userName + ". You are about to play a game of 'Rock, Paper, Scissors, Lizard, SPOCK!' against the Computer >:).");
         System.out.println("You are going to start with 0 points. Gain a point after each win and first to 3 points win!");
         System.out.println("You: " + userPoints + " - " + CPUPoints + " :Computer");
@@ -17,7 +19,7 @@ public class RPSL_Spock {
         //System.out.println("Choose one of the following options: Rock, Paper, Scissors, Lizard, or Spock." + " Please keep it in all lowercase!");
 
 
-                System.out.println("You: " + userPoints + " - " + CPUPoints + " :Computer");
+        System.out.println("You: " + userPoints + " - " + CPUPoints + " :Computer");
         boolean isValid = false;
         while (!isValid) {
             try {
@@ -38,26 +40,27 @@ public class RPSL_Spock {
         System.out.println("Computer's choice: " + options[select]);
         // write conditionals for who wins and who gains points!!
         boolean points = false;
-        while(points); {
+        while (points) ;
+        {
             if (userMove.equals(random)) {
                 System.out.println("It's a tie, no points gained.");
             }
 
-        if(userPoints != 3 || CPUPoints != 3) {
-            System.out.println("Choose one of the following options: Rock, Paper, Scissors, Lizard, or Spock." + " Please keep it in all lowercase!");
-            userMove = scanner.nextLine().toLowerCase().trim();
+            if (userPoints != 3 || CPUPoints != 3) {
+                System.out.println("Choose one of the following options: Rock, Paper, Scissors, Lizard, or Spock." + " Please keep it in all lowercase!");
+                userMove = scanner.nextLine().toLowerCase().trim();
 
-            // Computer's input code:
-            // Naming the options for String output
-            random = new Random();
+                // Computer's input code:
+                // Naming the options for String output
+                random = new Random();
 
-            select = random.nextInt(options.length);
-            System.out.println("Computer's choice: " + options[select]);
+                select = random.nextInt(options.length);
+                System.out.println("Computer's choice: " + options[select]);
 
-            if (userMove.equals(random)) {
+                if (userMove.equals(random)) {
                     System.out.println("It's a tie, no points gained.");
                 }
-                    if
+                if
                 (userMove.equals("rock") && random.equals("scissors") ||
                         (userMove.equals("rock") && random.equals("lizard")) ||
                         (userMove.equals("paper") && random.equals("rock")) ||
@@ -68,13 +71,13 @@ public class RPSL_Spock {
                         (userMove.equals("lizard") && random.equals("spock")) ||
                         (userMove.equals("spock") && random.equals("rock")) ||
                         (userMove.equals("spock") && random.equals("scissors"))) {
-                System.out.println("you win!");
-                ++userPoints;
-                System.out.println("You: " + userPoints + " - " + CPUPoints + " :Computer");
-            } else {
-                System.out.println("you lost!");
-                ++CPUPoints;
-                System.out.println("You: " + userPoints + " - " + CPUPoints + " :Computer");
+                    System.out.println("you win!");
+                    ++userPoints;
+                    System.out.println("You: " + userPoints + " - " + CPUPoints + " :Computer");
+                } else {
+                    System.out.println("you lost!");
+                    ++CPUPoints;
+                    System.out.println("You: " + userPoints + " - " + CPUPoints + " :Computer");
                 }
 
                 if (userPoints == 3) {
@@ -82,9 +85,7 @@ public class RPSL_Spock {
                 } else if (CPUPoints == 3) {
                     System.out.println("Winner! The Computer has triumphed over your skill");
                 }
-        }
+            }
         }
     }
-
-
-    }
+}
